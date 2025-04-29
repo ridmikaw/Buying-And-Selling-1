fd88533a9d120bab4fc2fb05a437c67d41e60a93
Ridmi Ranasinghe <it22088246@my.sliit.lk>
2025-04-30 00:13:27 +0530

    modify

--- supplier/src/component/Dashboard/Header/Nav/NavAvatar.js
+++ supplier/src/component/Dashboard/Header/Nav/NavAvatar.js
@@ -8,8 +8,8 @@ function NavAvatar() {
       <a
         className="nav-link nav-profile d-flex align-items-center pe-0"
         href="#1">
-        <img src="/images/my dp.jpg" alt="Profile" className="rounded-circle" />&nbsp;
-        <span className="d-none d-md-block">Ushan</span>
+        <img src="" alt="Profile" className="rounded-circle" />&nbsp;
+        <span className="d-none d-md-block">Ridmi</span>
       </a>
 
     </li>
